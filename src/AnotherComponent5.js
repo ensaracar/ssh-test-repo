01307ffe01b927a548087cf622df189d207e4cf6
Ensar Acar <ensar-acar@hotmail.com>
2023-03-31 14:42:40 +0300

    ++

---
+++ src/AnotherComponent5.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+const AnotherComponent5 = () =>
+    <div>AnotherComponent 5</div>;
+
+export default AnotherComponent5;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ import AnotherComponent from './AnotherComponent';
 import AnotherComponent2 from "./AnotherComponent2";
 import AnotherComponent3 from "./AnotherComponent3";
 import AnotherComponent4 from "./AnotherComponent4";
+import AnotherComponent5 from "./AnotherComponent5";
 
-export {TodaysDate, AnotherComponent, AnotherComponent2, AnotherComponent3, AnotherComponent4}
+export {TodaysDate, AnotherComponent, AnotherComponent2, AnotherComponent3, AnotherComponent4, AnotherComponent5}
