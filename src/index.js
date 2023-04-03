8906045b1e5ec5c24d86e5d4a1a9136c15b4b0d4
Ensar Acar <ensar-acar@hotmail.com>
2023-04-03 14:32:07 +0300

    ++

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import AnotherComponent2 from "./AnotherComponent2";
 import AnotherComponent3 from "./AnotherComponent3";
 import AnotherComponent4 from "./AnotherComponent4";
 import AnotherComponent5 from "./AnotherComponent5";
+import AnotherComponent6 from "./AnotherComponent6";
 
-export {TodaysDate, AnotherComponent, AnotherComponent2, AnotherComponent3, AnotherComponent4, AnotherComponent5}
+export {TodaysDate, AnotherComponent, AnotherComponent2, AnotherComponent3, AnotherComponent4, AnotherComponent5, AnotherComponent6}
